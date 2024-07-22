eaebb0e1738b97788049e90d9e7c0f1f5e7d5486
Georg Herison <georgherison@gmail.com>
2024-07-22 00:32:15 -0300

    starting packages, docker and configs

---
+++ src/main/java/com/Erigeo/CaptureDex/CaptureDexApplication.java
@@ -0,0 +1,13 @@
+package com.Erigeo.CaptureDex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureDexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureDexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Erigeo/CaptureDex/CaptureDexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Erigeo.CaptureDex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureDexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
